8ee0d27b38ed8eebca0dc616835af2c86df1c482
mgr <mgrojo@gmail.com>
2021-02-28 17:43:14 +0100

    Delete trailing whitespace

--- 02 Doodle Jump/doodle_jump.adb
+++ 02 Doodle Jump/doodle_jump.adb
@@ -19,32 +19,32 @@ procedure Doodle_Jump is
 
    xLimit : constant := 400;
    yLimit : constant := 533;
-   
+
    package RandomInt is new Ada.Numerics.Discrete_Random (sfInt32);
    generator : RandomInt.Generator;
 
    app : sfRenderWindow_Ptr := RenderWindow.create((xLimit, yLimit, 32), "Doodle Game!");
    e : Event.sfEvent;
-   
+
    t1 : sfTexture_Ptr := Texture.createFromFile("images/background.png");
    t2 : sfTexture_Ptr := Texture.createFromFile("images/platform.png");
    t3 : sfTexture_Ptr := Texture.createFromFile("images/doodle.png");
    sBackground, sPlat, sPers : sfSprite_Ptr := Sprite.create;
-   
+
    -- Dimensions of the sprites for calculating feet on platform.
    platformWidth : constant sfInt32 := sfInt32(Texture.getSize(t2).x);
    platformHeight : constant sfInt32 := sfInt32(Texture.getSize(t2).y);
    persHeight : constant := 70;
    persRight : constant := 50;
    persLeft : constant := 20;
-   
+
    platforms : array (sfInt32 range 0 .. 9) of Vector2.sfVector2i := (others => (0, 0));
    x, y : sfInt32 := 100;
    h : constant := 200;
    dx, dy : Float := 0.0;
-   
+
 begin
-   
+
    RandomInt.Reset (generator);
 
    RenderWindow.setFramerateLimit(app, 60);
@@ -56,13 +56,13 @@ begin
    Sprite.setTexture(sBackground, t1);
    Sprite.setTexture(sPlat, t2);
    Sprite.setTexture(sPers, t3);
-   
+
    for eachPlatform of platforms loop
       eachPlatform :=
         (x => randomInt.Random(generator) mod xLimit,
          y => randomInt.Random(generator) mod yLimit);
    end loop;
-   
+
    while RenderWindow.isOpen(app) = sfTrue loop
 
       while RenderWindow.PollEvent (app, event => e) = sfTrue loop
@@ -76,29 +76,29 @@ begin
          end case;
 
       end loop;
-      
+
       if Keyboard.isKeyPressed(Keyboard.sfKeyRight) then x := x + 3; end if;
       if Keyboard.isKeyPressed(Keyboard.sfKeyLeft)  then x := x - 3; end if;
-      
+
       dy := dy + 0.2;
       y := y + sfInt32 (dy);
       if y > 500 then
          dy := -10.0;
       end if;
-      
+
       if y < h then
          y := h;
          for eachPlatform of platforms loop
             eachPlatform.y := eachPlatform.y - sfInt32(dy);
-            
+
             if eachPlatform.y > yLimit then
                eachPlatform.y := 0;
                eachPlatform.x := randomInt.Random(generator) mod xLimit;
             end if;
-            
+
          end loop;
       end if;
-      
+
       for eachPlatform of platforms loop
          if x + persRight > eachPlatform.x and then
             x + persLeft < eachPlatform.x + platformWidth and then
@@ -109,20 +109,20 @@ begin
             dy := -10.0;
          end if;
       end loop;
-      
+
       Sprite.setPosition(sPers, (Float (x), Float (y)));
-      
+
       RenderWindow.drawSprite(app, sBackground);
-      
+
       for eachPlatform of platforms loop
          Sprite.setPosition(sPlat, (Float (eachPlatform.x), Float (eachPlatform.y)));
          RenderWindow.drawSprite(app, sPlat);
       end loop;
-      
+
       RenderWindow.drawSprite(app, sPers);
-      
+
       RenderWindow.display (app);
-      
+
    end loop;
-   
+
 end Doodle_Jump;
--- 03 Arkanoid/arkanoid.adb
+++ 03 Arkanoid/arkanoid.adb
@@ -13,7 +13,7 @@ with Sf.Window.Keyboard; use Sf.Window.Keyboard;
 with Ada.Numerics.Discrete_Random;
 
 procedure Arkanoid is
-   
+
    function isCollide (s1, s2 : sfSprite_Ptr) return Boolean is
       boundsS1 : aliased constant sfFloatRect := getGlobalBounds(s1);
       boundsS2 : aliased constant sfFloatRect := getGlobalBounds(s2);
@@ -22,15 +22,15 @@ procedure Arkanoid is
                          boundsS2'Access,
                          intersection => null) = sfTrue;
    end isCollide;
-   
+
    xLimit : constant := 520;
    yLimit : constant := 450;
-   
+
    app : sfRenderWindow_Ptr :=
      create ((xLimit, yLimit, 32), "Arkanoid Game!");
    t1, t2, t3, t4: sfTexture_Ptr;
    sBackground, sBall, sPaddle : sfSprite_Ptr := create;
-   
+
    numberOfRows : constant := 10;
    numberOfColumns : constant := 10;
    numberOfBlocks : constant := numberOfRows * numberOfColumns;
@@ -38,31 +38,31 @@ procedure Arkanoid is
    blocks : array (t_BlockId) of sfSprite_Ptr := (others => create);
    n : t_BlockId := t_BlockId'First;
    e : sfEvent;
-   
+
    type t_RandomY is range 2 .. 6;
    package RandomY is new Ada.Numerics.Discrete_Random (t_RandomY);
    generator : RandomY.Generator;
-   
+
    dx : Float := 6.0; dy : Float := 5.0;
    b : sfVector2f;
-   x, y : Float := 300.0; 
+   x, y : Float := 300.0;
 begin
-   
+
    RandomY.Reset (generator);
    setFramerateLimit(app, 50);
-   
+
    t1 := createFromFile("images/block01.png");
    t2 := createFromFile("images/background.jpg");
    t3 := createFromFile("images/ball.png");
    t4 := createFromFile("images/paddle.png");
-   
+
    setTexture(sBackground, t2);
    setTexture(sBall, t3);
    setTexture(sPaddle, t4);
 
    setPosition(sPaddle, (300.0, 440.0));
    setPosition(sBall, (300.0, 300.0));
-   
+
    for I in 1 .. numberOfColumns loop
       for J in 1 .. numberOfRows loop
          setTexture (blocks(n), t1);
@@ -72,16 +72,16 @@ begin
          end if;
       end loop;
    end loop;
-   
+
    while isOpen(app) = sfTrue loop
       while PollEvent (app, event => e) = sfTrue loop
-         
+
          if e.eventType = sfEvtClosed then
             Close (app);
          end if;
-         
+
       end loop;
-      
+
       move(sBall, (dx, 0.0));
       for eachBlock of blocks loop
          if isCollide(sBall, eachBlock) then
@@ -89,7 +89,7 @@ begin
             dx := -dx;
          end if;
       end loop;
-      
+
       move(sBall, (0.0, dy));
       for eachBlock of blocks loop
          if isCollide(sBall, eachBlock) then
@@ -105,37 +105,37 @@ begin
       if b.y < 0.0 or b.y > Float(yLimit) then
          dy := -dy;
       end if;
-      
+
       if isKeyPressed (sfKeyRight) then
          move(sPaddle, (6.0, 0.0));
       end if;
       if isKeyPressed (sfKeyLeft) then
          move(sPaddle, (-6.0, 0.0));
       end if;
-      
+
       if isCollide(sPaddle, sBall) then
          dy := Float(-RandomY.Random(Generator));
       end if;
-      
+
       clear(app);
       drawSprite(app, sBackground);
       drawSprite(app, sBall);
       drawSprite(app, sPaddle);
-      
+
       for eachBlock of blocks loop
          drawSprite(app, eachBlock);
       end loop;
-      
+
       display(app);
-      
+
    end loop;
-   
+
    destroy(sBackground);
    destroy(sBall);
    destroy(sPaddle);
    for eachBlock of blocks loop
       destroy(eachBlock);
-   end loop;   
+   end loop;
    destroy(t1);
    destroy(t2);
    destroy(t3);
